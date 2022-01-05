03945342d0631bcd2362a8150bb205a87e97df6e
CHUNG JEON <46134771+C4stleB@users.noreply.github.com>
2022-01-05 23:33:22 +0900

    added detailed function

--- src/server.js
+++ src/server.js
@@ -1,33 +1,129 @@
-import http from "http";
-import SocketIO from "socket.io";
 import express from "express";
+// import WebSocket from "ws";
+import SocketIO from "socket.io";
+import http from "http";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/*", (req, res) => {
+  res.redirect("/");
+});
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+let roomObjArr = [
+  // {
+  //   roomName,
+  //   currentNum,
+  //   users: [
+  //     {
+  //       socketId,
+  //       nickname,
+  //     },
+  //   ],
+  // },
+];
+const MAXIMUM = 5;
+
 wsServer.on("connection", (socket) => {
-    socket.on("join_room", (roomName) => {
-        socket.join(roomName);
-        socket.to(roomName).emit("welcome");
+  let myRoomName = null;
+  let myNickname = null;
+
+  socket.on("join_room", (roomName, nickname) => {
+    myRoomName = roomName;
+    myNickname = nickname;
+
+    let isRoomExist = false;
+    let targetRoomObj = null;
+
+    for (let i = 0; i < roomObjArr.length; ++i) {
+      if (roomObjArr[i].roomName === roomName) {
+        // Reject join the room
+        if (roomObjArr[i].currentNum >= MAXIMUM) {
+          socket.emit("reject_join");
+          return;
+        }
+
+        isRoomExist = true;
+        targetRoomObj = roomObjArr[i];
+        break;
+      }
+    }
+
+    // Create room
+    if (!isRoomExist) {
+      targetRoomObj = {
+        roomName,
+        currentNum: 0,
+        users: [],
+      };
+      roomObjArr.push(targetRoomObj);
+    }
+
+    //Join the room
+    targetRoomObj.users.push({
+      socketId: socket.id,
+      nickname,
     });
-    socket.on("offer", (offer, roomName) => {
-        socket.to(roomName).emit("offer", offer);
-    });
-    socket.on("answer", (answer, roomName) => {
-        socket.to(roomName).emit("answer", answer);
-    });
-    socket.on("ice", (ice, roomName) => {
-        socket.to(roomName).emit("ice", ice);
-      });
+    ++targetRoomObj.currentNum;
+
+    socket.join(roomName);
+    socket.emit("accept_join", targetRoomObj.users);
   });
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+  socket.on("offer", (offer, remoteSocketId, localNickname) => {
+    socket.to(remoteSocketId).emit("offer", offer, socket.id, localNickname);
+  });
+
+  socket.on("answer", (answer, remoteSocketId) => {
+    socket.to(remoteSocketId).emit("answer", answer, socket.id);
+  });
+
+  socket.on("ice", (ice, remoteSocketId) => {
+    socket.to(remoteSocketId).emit("ice", ice, socket.id);
+  });
+
+  socket.on("chat", (message, roomName) => {
+    socket.to(roomName).emit("chat", message);
+  });
+
+  socket.on("disconnecting", () => {
+    socket.to(myRoomName).emit("leave_room", socket.id, myNickname);
+
+    let isRoomEmpty = false;
+    for (let i = 0; i < roomObjArr.length; ++i) {
+      if (roomObjArr[i].roomName === myRoomName) {
+        const newUsers = roomObjArr[i].users.filter(
+          (user) => user.socketId != socket.id
+        );
+        roomObjArr[i].users = newUsers;
+        --roomObjArr[i].currentNum;
+
+        if (roomObjArr[i].currentNum == 0) {
+          isRoomEmpty = true;
+        }
+      }
+    }
+
+    // Delete room
+    if (isRoomEmpty) {
+      const newRoomObjArr = roomObjArr.filter(
+        (roomObj) => roomObj.currentNum != 0
+      );
+      roomObjArr = newRoomObjArr;
+    }
+  });
+});
+
+const handleListen = () => console.log(`✅ Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
